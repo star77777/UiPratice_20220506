0af43e89a335a7659a8f386f062e883a6e12852a
star <quf2525@naver.com>
2022-05-06 14:34:05 +0900

    Initial commit

---
+++ app/src/main/java/com/leejinsil/uipratice_20220506/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.leejinsil.uipratice_20220506
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
